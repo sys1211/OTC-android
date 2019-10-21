821cfa5be246fab543ca4d16bc503ce1212b88f3
sys1211 <18635503918@163.com>
2019-10-21 09:39:54 +0800

    修改线上地址

--- app/src/main/java/com/otcbase/merchant/MainActivity.java
+++ app/src/main/java/com/otcbase/merchant/MainActivity.java
@@ -281,7 +281,7 @@ public class MainActivity extends AppCompatActivity {
         wvOTC.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
 
         // 载入内容
-        wvOTC.loadUrl("https://dev.otcbase.com/mobile/account/sign-in");
+        wvOTC.loadUrl("https://www.otcbase.com/zh-CN/mobile/account/sign-in");
     }
 
     public void saveImage(String data) {
